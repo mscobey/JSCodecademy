276150c4e4589a4420bb1f3d5446c5c0531cc845
mscobey <126064568+mscobey@users.noreply.github.com>
2023-02-22 17:43:40 -0600

    Added word arrays

---
+++ MixedMessagesProject/mixedMessages.js
@@ -0,0 +1,11 @@
+const words1 = ['You','It','She','He','They'];
+const words2 = [' are',' was',' were',' has',' been'];
+const words3 = [' going',' wanting',' deciding',' planning',' running'];
+const words4 = [' to',' from',' a',' the',' as'];
+const words5 = [' a',' school',' love',' work',' house'];
+const words6 = [' feverishly',' quickly',' slowly',' drunkenly',' cheerfully'];
+const punc = ['!','.','?','!?!','...'];
+
+
+
+
