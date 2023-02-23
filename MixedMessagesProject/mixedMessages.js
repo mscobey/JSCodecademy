1ad833fdcb381f4d4cfe1a1782bf3d1fa1b11d69
mscobey <126064568+mscobey@users.noreply.github.com>
2023-02-22 18:18:55 -0600

    Array for words, for loop to generate string

--- MixedMessagesProject/mixedMessages.js
+++ MixedMessagesProject/mixedMessages.js
@@ -4,8 +4,12 @@ const words3 = [' going',' wanting',' deciding',' planning',' running'];
 const words4 = [' to',' from',' a',' the',' as'];
 const words5 = [' a',' school',' love',' work',' house'];
 const words6 = [' feverishly',' quickly',' slowly',' drunkenly',' cheerfully'];
-const punc = ['!','.','?','!?!','...'];
+const words =[words1,words2,words3,words4,words5,words6];
 
-
+let output='';
+for(let i=0;i<words.length;i++){
+    output+=words[i][Math.floor(Math.random()*words[i].length)];
+}
+console.log(output);
 
 
